7fb46aec9d7f034da3477047cab665b5d79a1d65
Byrapuneni Mahima Chowdary <s545519@nwmissouri.edu>
2022-12-04 23:34:56 -0600

    Add files via upload

---
+++ Goal3/MyMapper3.java
@@ -0,0 +1,19 @@
+package Goal3;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class MyMapper3 extends Mapper<LongWritable, Text, Text, IntWritable>{
+    
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        String[] row = value.toString().split("\t");
+        context.write(new Text(row[0]), new IntWritable(Integer.parseInt(row[3])));
+    }
+    
+}
